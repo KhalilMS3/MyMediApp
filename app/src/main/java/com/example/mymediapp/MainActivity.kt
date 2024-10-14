9ab62b0968fabcfb30e4829d8a639c9df3ce1416
Dilbrino <dilbrinh@hiof.no>
2024-10-14 18:27:03 +0200

    creating user Firebase Authentication and Firestore for registering and loggin and storing user

--- app/src/main/java/com/example/mymediapp/MainActivity.kt
+++ app/src/main/java/com/example/mymediapp/MainActivity.kt
@@ -5,19 +5,20 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.*
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.*
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.mymediapp.ui.screens.ReminderScreen
 import com.example.mymediapp.ui.theme.AppTheme
-import com.example.mymediapp.viewmodel.ReminderViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
