5843625c738aa6ee7ed349b2dc3b63ae8e33753d
Firas.MK <78766937+firas-mk@users.noreply.github.com>
2024-11-06 21:22:25 +0100

    Modify Reminder and Time data classes. because firestore requires that data classes have a public no-argument constructor

--- app/src/main/java/com/example/mymediapp/model/Reminder.kt
+++ app/src/main/java/com/example/mymediapp/model/Reminder.kt
@@ -1,18 +1,24 @@
 package com.example.mymediapp.model
 
 import java.util.Date
+import java.util.UUID
+
 
 data class Reminder(
-    val medicineName: String,
-    val numberOfDoses: Int,        // Number of doses
-    val timeBetweenDoses: Int,     // Time between doses in hours
-    val startDate: Date,           // Start date for reminders
-    val endDate: Date,             // End date for the reminder period
-    val startTime: Time,           // Start time of the day for the first dose
-    val notes: String? = null      // Optional notes for the reminder
-)
+
+    val medicineName: String = "",
+    val numberOfDoses: Int = 0,        // Number of doses
+    val timeBetweenDoses: Int = 0,     // Time between doses in hours
+    val startDate: Date = Date(),      // Start date for reminders
+    val endDate: Date = Date(),        // End date for the reminder period
+    val startTime: Time = Time(),      // Start time of the day for the first dose
+    val notes: String? = null ,         // Optional notes for the reminder
+    //val id: String = "${medicineName}_${System.currentTimeMillis()}",
+    val id: String = UUID.randomUUID().toString()
+
+    )
 
 data class Time(
-    val hours: Int,
-    val minutes: Int
+    val hours: Int = 0,
+    val minutes: Int = 0
 )
