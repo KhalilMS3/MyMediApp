f1885b6dcba4c50a788e1148699e3f78bc7f2df7
Firas.MK <78766937+firas-mk@users.noreply.github.com>
2024-11-06 21:27:55 +0100

    In myMedicationsScreen.kt  replace the dummyMedicines with reminders from the ViewModel

--- app/src/main/java/com/example/mymediapp/ui/screens/myMedicationsScreen.kt
+++ app/src/main/java/com/example/mymediapp/ui/screens/myMedicationsScreen.kt
@@ -4,6 +4,8 @@ package com.example.mymediapp.ui.screens
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
@@ -25,35 +27,17 @@ fun myMedicationsScreen(navController: NavController, viewModel: ReminderViewMod
     val medicines = viewModel.medicineResults.observeAsState(initial = emptyList()).value
     val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
 
-    // Dummy list until using database
-    val dummyMedicines = listOf(
-        Reminder(
-            medicineName = "Aspirin",
-            numberOfDoses = 2,
-            timeBetweenDoses = 6,
-            startDate = dateFormat.parse("2024-10-17") ?: Date(),
-            endDate = dateFormat.parse("2024-11-17") ?: Date(),
-            startTime = Time(10, 0),
-            notes = "Take after meal"
-        ),
-        Reminder(
-            medicineName = "Ibuprofen",
-            numberOfDoses = 1,
-            timeBetweenDoses = 8,
-            startDate = dateFormat.parse("2024-10-18") ?: Date(),
-            endDate = dateFormat.parse("2024-11-18") ?: Date(),
-            startTime = Time(9, 0),
-            notes = "Morning only"
-        )
-    )
-
-    // Screen content
-    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            //.verticalScroll(rememberScrollState())
+    ) {
         Text(text = "My Medications", modifier = Modifier.padding(bottom = 8.dp))
 
         // Display the list of reminders using LazyColumn
         LazyColumn(modifier = Modifier.fillMaxSize()) {
-            items(dummyMedicines) { reminder ->  // items(reminders)
+            items(reminders.value) { reminder ->
                 ReminderItem(reminder = reminder)
             }
         }
